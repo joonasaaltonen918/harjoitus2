f2d79117acbeb4e7232d4057eb97f4457254ac2a
joonas <joonas918@gmail.com>
2021-09-17 10:17:47 +0300

    adaf

--- EsimerkkiLaskin/EsimerkkiLaskin/Program.cs
+++ EsimerkkiLaskin/EsimerkkiLaskin/Program.cs
@@ -6,7 +6,7 @@ namespace EsimerkkiLaskin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Laskin ohjelman toinen muutos");
         }
     }
 }
